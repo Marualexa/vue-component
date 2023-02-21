94d112874e35234cf6a7c9abfb8e0f401c2dd3d5
Mariana <contrerasomanam@gmail.com>
2023-02-21 15:40:06 -0500

    navegacion de programacion

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,27 @@
 <script>
 export default {
-    data() {
-        return
-        username: "";
+  data() {
+    return {
+      username: ''
+    };
+  },
+  methods: {
+    iniciar() {
+      this.$router.push({
+        path: '/chats',
+        query: {
+          variable1: 1
+        }
+      })
     }
-}
+  },
+};
 </script>
 
 <template>
   <main>
     <h3>Introduce tu nombre de usuario</h3>
-    <input placeholder="username" v-model="username">
+    <input placeholder="username" v-model="username" />
+    <button @click="iniciar">Iniciar sección</button>
   </main>
 </template>
-
