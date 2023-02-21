171f7e70c72ffcfb87e9859684e94b243195e154
Mariana <contrerasomanam@gmail.com>
2023-02-21 10:58:34 -0500

    rutas anidadas

--- src/views/ChatsView.vue
+++ src/views/ChatsView.vue
@@ -1,28 +1,28 @@
 <template>
     <div>
-        <aside>
-            <div v-for="chat in chats" :key="chat.id">
-                <router-link :to="`/chats/${chat.id}`">
-                    {{ chat.name }}
-                </router-link>
-            </div>
-        </aside>
-        <article>
-            Mensaje
-        </article>
+      <aside>
+        <div v-for="chat in chats" :key="chat.id">
+          <router-link :to="`/chats/${chat.id}`">
+            {{ chat.name }}
+          </router-link>
+        </div>
+      </aside>
+      <article>
+        <router-view />
+      </article>
     </div>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  export default {
     data() {
-        return {
-            chats: [
-                { id: 1, name: 'Ximena'},
-                { id: 2, name: 'Daniel'},
-                { id: 3, name: 'Miguel'},
-            ]
-        }
+      return {
+        chats: [
+          { id: 1, name: 'Ximena' },
+          { id: 2, name: 'Daniel' },
+          { id: 3, name: 'Miguel' }
+        ]
+      }
     }
-}
-</script>
+  }
+  </script>
